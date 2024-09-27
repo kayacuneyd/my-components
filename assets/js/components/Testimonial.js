c69644c825c1531cb3f1895c3588263646d421ec
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2024-09-27 23:22:33 +0000

    google reviews added

--- assets/js/components/Testimonial.js
+++ assets/js/components/Testimonial.js
@@ -1,23 +1,12 @@
 // assets/js/components/Testimonial.js
 export function Testimonial() {
   return `
-        <div class="py-5 bg-light">
+      <section class="py-5 bg-light" id="scroll-target">
             <div class="container px-5 my-5">
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-lg-10 col-xl-7">
-                        <div class="text-center">
-                            <div class="fs-4 mb-4 fst-italic">
-                            "A ship is safe in the harbor, but it will never know the meaning of freedom unless it sails. The waves will challenge you, but remember, it’s not the wind that brings you to shore—it’s your sails."
-                            </div>
-                            <div class="d-flex align-items-center justify-content-center">
-                                <div class="fw-bold">
-                                    Anonim
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div class="row gx-5 align-items-center">
+                <iframe src='https://widgets.sociablekit.com/google-reviews/iframe/25468706' frameborder='0' width='100%' height='600px'></iframe>
+              </div>
             </div>
-        </div>
+          </section>
     `;
 }
